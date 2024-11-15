d0490179d451f65bd2d0b1ad0fefa12292fb73b9
Baskoro Ramadhan <baskoror725@gmail.com>
2024-11-15 14:18:23 +0700

    change header style with tailwind css help

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import { styled } from 'styled-components';
-
 import logo from '../assets/logo.png';
+/* import { styled } from 'styled-components'; */
 
 
+/* 
 const StyledHeader = styled.header`
 & {
   display: flex;
@@ -46,14 +46,23 @@ const StyledHeader = styled.header`
     font-size: 2.25rem;
   }
 }
-`
+` */
 
-export default function Header() {
+/* export default function Header() {
   return (
     <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <img src={logo} alt="A canvas" />
+    <h1>ReactArt</h1>
+    <p>A community of artists and art-lovers.</p>
     </StyledHeader>
-  );
-}
+    );
+  }
+  */
+export default function Header() {
+  return (
+    <header className='flex flex-col items-center mt-8 mb-16 '>
+      <img src={logo} alt="A canvas" className='object-contain mb-8 w-44 h-44'/>
+      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
+    </header>
+  )};
